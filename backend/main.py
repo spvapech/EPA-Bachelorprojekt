b05dc7497bd4cd0152e08a165f1c8dd48b732a49
Vaios <pechlevanidis.vaios@gmail.com>
2026-04-11 13:15:02 +0200

    Use env variable for API URL

--- backend/main.py
+++ backend/main.py
@@ -29,7 +29,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://epa-bachelorprojekt.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
